594182fd737447417bfae761570600d0ed9ac57b
benki-ng <ben.king32@gmail.com>
2020-08-19 22:38:12 +0100

    Added initial testing files

---
+++ homekit-door.ino
@@ -0,0 +1,112 @@
+
+/*
+ * switch.ino
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ *
+ * HAP section 8.38 Switch
+ * An accessory contains a switch.
+ *
+ * This example shows how to:
+ * 1. define a switch accessory and its characteristics (in my_accessory.c).
+ * 2. get the switch-event sent from iOS Home APP.
+ * 3. report the switch value to HomeKit.
+ *
+ * You should:
+ * 1. read and use the Example01_TemperatureSensor with detailed comments
+ *    to know the basic concept and usage of this library before other examples。
+ * 2. erase the full flash or call homekit_storage_reset() in setup()
+ *    to remove the previous HomeKit pairing storage and
+ *    enable the pairing with the new accessory of this new HomeKit example.
+ */
+
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#include "wifi_info.h"
+
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+
+void setup() {
+	Serial.begin(115200);
+	wifi_connect(); // in wifi_info.h
+	//homekit_storage_reset(); // to remove the previous HomeKit pairing storage when you first run this new HomeKit example
+	my_homekit_setup();
+}
+
+void loop() {
+	my_homekit_loop();
+	delay(10);
+}
+
+//==============================
+// HomeKit setup and loop
+//==============================
+
+// access your HomeKit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+
+extern "C" homekit_characteristic_t cha_lock_target_state;
+extern "C" homekit_characteristic_t cha_lock_current_state;
+
+static uint32_t next_heap_millis = 0;
+
+#define PIN_SWITCH D0
+
+bool test = true;
+//Called when the switch value is changed by iOS Home APP
+void cha_switch_on_setter(const homekit_value_t target) {
+/*
+    lock_state_unsecured = 0,
+    lock_state_secured = 1,
+    lock_state_jammed = 2,
+    lock_state_unknown = 3,
+ */
+ bool targetval = target.bool_value;
+ 
+ cha_lock_current_state.value.uint8_value = (targetval)?1:0;
+ LOG_D("Current state: %x", cha_lock_current_state.value);
+ homekit_characteristic_notify(&cha_lock_current_state, cha_lock_current_state.value);
+  digitalWrite(PIN_SWITCH,  cha_lock_current_state.value.bool_value);
+  
+  cha_lock_current_state.value.uint8_value = false;
+  homekit_characteristic_notify(&cha_lock_current_state, cha_lock_current_state.value);
+
+//  delay(10000);
+
+  digitalWrite(PIN_SWITCH,  cha_lock_current_state.value.bool_value);
+}
+
+void my_homekit_setup() {
+	pinMode(PIN_SWITCH, OUTPUT);
+	digitalWrite(PIN_SWITCH, HIGH);
+
+	//Add the .setter function to get the switch-event sent from iOS Home APP.
+	//The .setter should be added before arduino_homekit_setup.
+	//HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
+	//Maybe this is a legacy design issue in the original esp-homekit library,
+	//and I have no reason to modify this "feature".
+  cha_lock_target_state.setter = cha_switch_on_setter;
+  
+	arduino_homekit_setup(&config);
+
+	//report the switch value to HomeKit if it is changed (e.g. by a physical button)
+	//bool switch_is_on = true/false;
+	 homekit_characteristic_notify(&cha_lock_current_state, cha_lock_current_state.value);
+}
+
+void my_homekit_loop() {
+	arduino_homekit_loop();
+	const uint32_t t = millis();
+	if (t > next_heap_millis) {
+		// show heap info every 5 seconds
+   
+    cha_lock_current_state.value.uint8_value = (uint8)0;
+  homekit_characteristic_notify(&cha_lock_current_state, cha_lock_current_state.value);
+   
+		next_heap_millis = t + 5 * 1000;
+		LOG_D("Free heap: %d, HomeKit clients: %d",
+				ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+	}
+}
